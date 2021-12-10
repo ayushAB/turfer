a62fcf722a82c750bbec4629519b0c022957950b
ayushAB <ayushbante6@gmail.com>
2021-12-10 23:50:13 +0530

    Added listing design of turfs

---
+++ resources/js/Components/Turf.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="flex flex-col items-center group">
+        <div
+            class="
+                w-full
+                flex
+                shadow-md
+                border border-gray-200
+                overflow-hidden
+                transition-all
+                duration-500
+                group-hover:-translate-y-4
+                cursor-pointer
+                z-20
+                bg-white
+            "
+        >
+            <div class="w-2/5 overflow-hidden">
+                <img
+                    class="object-cover"
+                    src="https://images.unsplash.com/photo-1638628578277-94c775a02e61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1546&q=80"
+                />
+            </div>
+            <div class="flex-1 flex flex-col bg-white justify-between px-4 py-3">
+                <div>
+                    <h1 class="text-xl text-gray-800">Berlin</h1>
+                    <div class="flex items-center">
+                        <h3 class="line-through text-gray-400">$390</h3>
+                        <h3 class="ml-1 text-sm text-gray-400">$290</h3>
+                    </div>
+                </div>
+                <div>
+                    <div class="flex items-center">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5 text-gray-600"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                        <span class="ml-1 text-base text-gray-400"
+                            >Germany</span
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div
+            class="
+                bg-gray-700
+                max-w-2xl
+                -translate-y-8
+                transition-all
+                opacity-0
+                group-hover:opacity-100
+                group-hover:-translate-y-6
+                duration-700
+                text-white text-center
+                w-full
+                rounded
+                font-medium
+                py-3
+                pb-1
+            "
+        >
+            View
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -18,7 +18,7 @@
                             h-12
                             w-12
                             rounded-full
-                            bg-white bg-opacity-80
+                            bg-white bg-opacity-90
                             flex
                             justify-center
                             items-center
@@ -33,7 +33,7 @@
                             h-12
                             w-12
                             rounded-full
-                            bg-white bg-opacity-80
+                            bg-white bg-opacity-90
                             flex
                             justify-center
                             items-center
@@ -46,23 +46,67 @@
                         src="https://images.unsplash.com/photo-1588415759532-d457888bd1cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                     />
                 </div>
-                <div class=" absolute -bottom-7 left-72 rounded w-full flex -mt-8 z-10 bg-white max-w-2xl px-2 py-3">
-                        <input class="flex-1 border border-gray-200 focus:outline-none focus:ring-0 focus:ring-transparent" type="text" placeholder="Search" />
-                        <div class="flex justify-center items-center bg-gray-200 px-3 py-2">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-5 w-5"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                        </div>
+                <div
+                    class="
+                        absolute
+                        -bottom-7
+                        left-72
+                        rounded
+                        w-full
+                        flex
+                        -mt-8
+                        z-10
+                        bg-white
+                        max-w-2xl
+                        px-2
+                        py-3
+                    "
+                >
+                    <input
+                        class="
+                            flex-1
+                            border border-gray-200
+                            focus:outline-none
+                            focus:ring-0
+                            focus:ring-transparent
+                        "
+                        type="text"
+                        placeholder="Search"
+                    />
+                    <div
+                        class="
+                            flex
+                            justify-center
+                            items-center
+                            bg-gray-200
+                            px-3
+                            py-2
+                        "
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
                     </div>
+                </div>
+            </div>
+            <div class="max-w-7xl mt-20 mx-auto sm:px-6 lg:px-8  py-2 relative border-b border-gray-200">
+                <h1 class="text-2xl text-gray-400">Turfs</h1>
+            </div>
+            <div class="max-w-7xl grid grid-cols-2 gap-6 mt-10 mx-auto sm:px-6 lg:px-8 relative">
+                <Turf />
+                <Turf />
+                <Turf />
+                <Turf />
+                <Turf />
             </div>
         </div>
     </app-layout>
@@ -72,11 +116,13 @@
 import { defineComponent } from "vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Welcome from "@/Jetstream/Welcome.vue";
+import Turf from "@/Components/Turf.vue";
 
 export default defineComponent({
     components: {
         AppLayout,
         Welcome,
+        Turf,
     },
 });
 </script>
