e776c34da63a5adba9344463c37c71d0d1c59e30
ayushAB <ayushbante6@gmail.com>
2021-12-10 22:58:56 +0530

    Set layout for user end

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,30 +1,82 @@
 <template>
     <app-layout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 relative">
+                <div
+                    class="
+                        bg-white
+                        overflow-hidden
+                        shadow-2xl
+                        sm:rounded-lg
+                        relative
+                    "
+                >
+                    <div
+                        class="
+                            absolute
+                            sm:left-10 sm:top-40
+                            h-12
+                            w-12
+                            rounded-full
+                            bg-white bg-opacity-80
+                            flex
+                            justify-center
+                            items-center
+                        "
+                    >
+                        {{ "<" }}
+                    </div>
+                    <div
+                        class="
+                            absolute
+                            sm:right-10 sm:top-40
+                            h-12
+                            w-12
+                            rounded-full
+                            bg-white bg-opacity-80
+                            flex
+                            justify-center
+                            items-center
+                        "
+                    >
+                        {{ ">" }}
+                    </div>
+                    <img
+                        class="max-h-96 w-full object-cover"
+                        src="https://images.unsplash.com/photo-1588415759532-d457888bd1cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                    />
                 </div>
+                <div class=" absolute -bottom-7 left-72 rounded w-full flex -mt-8 z-10 bg-white max-w-2xl px-2 py-3">
+                        <input class="flex-1 border border-gray-200 focus:outline-none focus:ring-0 focus:ring-transparent" type="text" placeholder="Search" />
+                        <div class="flex justify-center items-center bg-gray-200 px-3 py-2">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="h-5 w-5"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        </div>
+                    </div>
             </div>
         </div>
     </app-layout>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
-    import AppLayout from '@/Layouts/AppLayout.vue'
-    import Welcome from '@/Jetstream/Welcome.vue'
+import { defineComponent } from "vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Welcome from "@/Jetstream/Welcome.vue";
 
-    export default defineComponent({
-        components: {
-            AppLayout,
-            Welcome,
-        },
-    })
+export default defineComponent({
+    components: {
+        AppLayout,
+        Welcome,
+    },
+});
 </script>
